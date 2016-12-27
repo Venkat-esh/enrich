dcb9fda15ef56360091322e62d4b20fd2a8cbdf6
Venkat-esh <venkatesh.gravi@gmail.com>
2016-12-27 18:37:05 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,rev,k;
+  printf("Enter the number");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    k=n%10;
+    rev=rev*10+k;
+    n=n/10;
+  }
+  printf("\n The reverse of the number is %d",rev);
+  getch();
+}
