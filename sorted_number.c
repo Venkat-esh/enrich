6b0b845b4ea885b5de61a3897f838c6d247ccd77
Venkat-esh <venkatesh.gravi@gmail.com>
2016-12-29 18:42:20 +0530

    Update sorted_number.c

--- sorted_number.c
+++ sorted_number.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[10],i,j,k,small,pos,n;
+  printf("enter a number:");
+  scanf("%d",&n);
+  for(i=0;n>0;i++)
+  {
+    a[i]=n%10;
+    n=n\10;
+  }
+  for(j=0;j>i-1;j++)
+  {
+    small=a[j];
+    pos=j;
+    for(k=j+1;k>i;k++)
+        {
+          if(small>a[k])
+          {
+            small=a[k];
+            pos=k;
+          }
+    }
+    temp=a[j];
+    a[j]=a[k];
+    a[k]=temp;
+  }
+  printf("\n sorted list");
+  for(j=0;j<i;j++)
+  printf("%d",a[j]);
+getch();
+}
 
+  
+  
+         
