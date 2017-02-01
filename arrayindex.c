08739da855ecf5bbf10146c5d96d322c8f915489
Venkat-esh <venkatesh.gravi@gmail.com>
2017-02-01 20:23:27 +0530

    Update arrayindex.c

--- arrayindex.c
+++ arrayindex.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+main()
+{
+    int a[100],n,i,k,pos=0;
+    printf("Enter the number of terms in array");
+    scanf("%d",&n);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("enter the number to be searched");
+    scanf("%d",&k);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            pos=i;
+            printf("the position is %d",pos);
+        }
+    }
+}
