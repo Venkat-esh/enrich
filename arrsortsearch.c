22b1cefceda19923daca59345b819189669006f7
Venkat-esh <venkatesh.gravi@gmail.com>
2017-02-02 22:32:26 +0530

    Update arrsortsearch.c

--- arrsortsearch.c
+++ arrsortsearch.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 main()
 {
-    int a[100],i,n,k,pos=0,temp=0,j;
+    int a[100],i,n,k,pos=0,temp=0,j=0;
     printf("enter the number of terms");
     scanf("%d",&n);
     printf("\n");
     printf("enter the array elements \n");
-    for(i=0;i<n;i++)
+    for(i=1;i<=n;i++)
     {
         scanf("%d",&a[i]);
     }
     printf("enter the number to be searched\n");
     scanf("%d",&k);
-    for(i=0;i<n;i++)
+    for(i=1;i<=n;i++)
     {
         if(a[i]==k)
         {
             pos++;
-            break;
+            continue;
         }
     }
     if(pos==0)
@@ -25,20 +25,20 @@ main()
         printf("term not found\n");
     }
     else
-    {   while(j<n)
+    {   while(j<=n)
         {
-        for(i=0;i<n;i++)
-        {
-            if(a[i]>a[i+1])
+            for(i=0;i<n;i++)
             {
-                temp=a[i];
-                a[i]=a[i+1];
-                a[i+1]=temp;
+                if(a[i]>a[i+1])
+                {
+                    temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+                }
             }
+            j++;
         }
-        j++;
-        }
-        for(i=1;i<=n;i++)
+        for(i=0;i<n;i++)
         {
             if(a[i]==k)
             {
