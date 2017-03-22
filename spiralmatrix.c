1472744715dfaf8f7c088c2ea32711d28ba8fe55
Venkat-esh <venkatesh.gravi@gmail.com>
2017-03-22 18:22:52 +0530

    Update spiralmatrix.c

--- spiralmatrix.c
+++ spiralmatrix.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 main()
 {
-	int a[10][10],i,j,k=1,n;
+	int a[10][10],i,j,k=1,n,m;
 	printf("enter the number of rows\n");
 	scanf("%d",&n);
+	printf("enter the number of columns\n");
+	scanf("%d",&m);
 	for(i=0;i<n;i++)
 	{
-		for(j=0;j<n;j++)
+		for(j=0;j<m;j++)
 		{
 			if(i%2==0)
 			{
@@ -15,14 +17,14 @@ main()
 			}
 			else
 			{
-				a[i][n-(j+1)]=k;
+				a[i][m-(j+1)]=k;
 				k++;
 			}
 		}
 	}
 	for(i=0;i<n;i++)
 	{
-		for(j=0;j<n;j++)
+		for(j=0;j<m;j++)
 		{
 			printf("\t%d",a[i][j]);
 		}
