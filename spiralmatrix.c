3c18018be857c1843d5def52e8e351bb0b4666f4
Venkat-esh <venkatesh.gravi@gmail.com>
2017-03-22 18:16:44 +0530

    Create spiralmatrix.c

---
+++ spiralmatrix.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+	int a[10][10],i,j,k=1,n;
+	clrscr();
+	printf("enter the number of rows");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i%2==0)
+			{
+				a[i][j]=k;
+				k++;
+			}
+			else
+			{
+				a[i][n-(j+1)]=k;
+				k++;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("\t%d",a[i][j]);
+		}
+		printf("\n");
+	}
+}
