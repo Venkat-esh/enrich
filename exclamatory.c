6af1e8f8ae828f305f8606c32d67fdd33869b2ef
Venkat-esh <venkatesh.gravi@gmail.com>
2017-08-22 18:34:39 +0530

    Update exclamatory.c

--- exclamatory.c
+++ exclamatory.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+main()
+{
+	int n,i,j,m,k,l,c;
+	scanf("%d",&n);
+	m=n/2;
+	c=1;
+	for(i=0;i<n/2+1;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			printf("!");
+		}
+		for(j=0;j<c;j++)
+		{
+			printf("*");
+		}
+		for(j=0;j<m;j++)
+		{
+			printf("!");
+		}
+		c=c+2;
+		m--;
+	  }
+return 0;
+}
